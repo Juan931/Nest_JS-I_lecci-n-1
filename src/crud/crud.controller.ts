719c3e8dc7e647a61c757aaef4b0fb93256e017c
juand <juandavidds@.es>
2021-09-01 09:32:14 -0500

    Task 3: Usando el código de respuesta correcta

---
+++ src/crud/crud.controller.ts
@@ -0,0 +1,35 @@
+import { Controller,Get,Post,Put,Delete,HttpStatus,Res, HttpCode } from '@nestjs/common';
+import { Response, response } from 'express';
+
+@Controller('crud')
+export class CrudController {
+   
+    @Get()
+    getCrud(@Res() response:Response):Response{
+      return response.status(HttpStatus.OK, ).send({//CODE 200
+          name:"get"
+      })
+    }
+    
+    @Post()
+    postCrud(@Res() response:Response):Response{
+        return response.status(HttpStatus.ACCEPTED).send({//CODE 202
+            name:"post"
+        })
+      }
+    
+    @Put()
+    putCrud(@Res() response:Response):Response{
+        return response.status(HttpStatus.CONFLICT).send({//CODE 409
+            name:"put"
+        })
+      }  
+
+    @Delete()
+    deleteCrud(@Res() response:Response):Response{
+        return response.status(HttpStatus.INTERNAL_SERVER_ERROR).send({//CODE 500
+            name:"delete"
+        })
+      }    
+
+}
